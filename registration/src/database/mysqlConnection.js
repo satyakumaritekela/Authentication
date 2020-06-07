7f5bff19c00f06baeb748b369f7ce630487d2d0e
Satya Kumar Itekela <st798799@dal.ca>
2020-06-07 20:50:55 -0300

    Added services

---
+++ registration/src/database/mysqlConnection.js
@@ -0,0 +1,18 @@
+const mysql = require("mysql");
+
+const connection = mysql.createConnection({
+  host: "127.0.0.1",
+  user: "root",
+  password: "Satti@18",
+  database: "db_assignment_2",
+});
+
+connection.connect((err) => {
+  if (!err) {
+    console.log("Database is connected ...");
+  } else {
+    console.log("Error connecting database ...");
+  }
+});
+
+module.exports = connection;
